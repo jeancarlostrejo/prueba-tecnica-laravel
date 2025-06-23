662d4703f6da2af19105abf9ea91ca26de724bc6
Jean Carlos Trejo <jeancarlostrejo19@gmail.com>
2025-06-23 19:10:09 -0400

    update profile information page

--- resources/views/profile/partials/update-profile-information-form.blade.php
+++ resources/views/profile/partials/update-profile-information-form.blade.php
@@ -47,6 +47,48 @@
             @endif
         </div>
 
+        {{-- Identifier --}}
+        <div>
+            <x-input-label for="identifier" :value="__('Identifier')" />
+            <x-text-input id="identifier" name="identifier" type="text" class="mt-1 block w-full" :value="old('identifier', $user->identifier)" required autofocus autocomplete="identifier" />
+            <x-input-error class="mt-2" :messages="$errors->get('identifier')" />
+        </div>
+
+        {{-- Phone --}}
+        <div>
+            <x-input-label for="phone" :value="__('Phone')" />
+            <x-text-input id="phone" name="phone" type="text" class="mt-1 block w-full" :value="old('phone', $user->phone)" required autofocus autocomplete="phone" />
+            <x-input-error class="mt-2" :messages="$errors->get('phone')" />
+        </div>
+
+        {{-- DNI --}}
+        <div>
+            <x-input-label for="dni" :value="__('Dni')" />
+            <x-text-input id="dni" name="dni" type="text" class="mt-1 block w-full" :value="old('dni', $user->dni)" required autofocus autocomplete="dni" />
+            <x-input-error class="mt-2" :messages="$errors->get('dni')" />
+        </div>
+
+        {{-- birth_date --}}
+        <div>
+            <x-input-label for="birth_date" :value="__('Birth Date')" />
+            <x-text-input id="birth_date" name="birth_date" type="text" class="mt-1 block w-full" :value="old('birth_date', $user->birth_date->format('d-m-Y'))" required autofocus autocomplete="birth_date" />
+            <x-input-error class="mt-2" :messages="$errors->get('birth_date')" />
+        </div>
+
+        {{-- role --}}
+        <div>
+            <x-input-label for="role" :value="__('Role')" />
+            <x-text-input id="role" name="role" type="text" class="mt-1 block w-full" :value="old('role', $user->role)" required autofocus autocomplete="role" />
+            <x-input-error class="mt-2" :messages="$errors->get('role')" />
+        </div>
+
+        {{-- city --}}
+        <div>
+            <x-input-label for="city" :value="__('City')" />
+            <x-text-input id="city" name="city_id" type="text" class="mt-1 block w-full" :value="old('city_id', $user->city->name)" required autofocus autocomplete="city" />
+            <x-input-error class="mt-2" :messages="$errors->get('city_id')" />
+        </div>
+
         <div class="flex items-center gap-4">
             <x-primary-button>{{ __('Save') }}</x-primary-button>
 
