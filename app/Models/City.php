6a6bcecd7fc6a325e61edc338d30ef2c59412de8
Jean Carlos Trejo <jeancarlostrejo19@gmail.com>
2025-06-23 16:53:29 -0400

    add migrations and model to country, state and city

---
+++ app/Models/City.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasOneThrough;
+
+class City extends Model
+{
+    protected $fillable = [
+        'name',
+        'state_id'
+    ];
+
+    public function state(): BelongsTo
+    {
+        return $this->belongsTo(State::class);
+    }
+
+    public function country(): HasOneThrough
+    {
+        return $this->hasOneThrough(Country::class, State::class);
+    }
+}
