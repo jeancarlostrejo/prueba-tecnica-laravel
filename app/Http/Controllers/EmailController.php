a6d017cf52bb2f535fced772a03545f37a1cb715
Jean Carlos Trejo <jeancarlostrejo19@gmail.com>
2025-06-24 00:49:04 -0400

    add email management views and validation request for store email

--- app/Http/Controllers/EmailController.php
+++ app/Http/Controllers/EmailController.php
@@ -2,13 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\StoreEmailRequest;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\View\View;
 
 class EmailController extends Controller
 {
     public function index()
     {
+        return view('emails.index');
     }
 
     public function create(): View
@@ -16,8 +19,10 @@ class EmailController extends Controller
         return view('emails.create');
     }
 
-    public function store(Request $request)
+    public function store(StoreEmailRequest $request)
     {
-        dd($request->all());
+        Auth::user()->emails()->create($request->validated());
+
+        return redirect()->route('emails.index')->with('success', 'Email sent successfully!');
     }
 }
---
+++ resources/views/emails/index.blade.php
@@ -0,0 +1,23 @@
+<x-app-layout>
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+                {{-- Dismiss Alert --}}
+                @if (session('success'))
+                    <div x-data="{ open: true }" x-show="open"
+                        class="relative bg-green-100 border border-green-400 text-green-700 px-4 py-3 rounded"
+                        role="alert">
+                        <strong class="font-bold">{{ session('success') }}</strong>
+                        <button @click="open = false" class="absolute top-0 bottom-0 right-0 px-4 py-3 text-green-700">
+                            <span class="text-lg font-bold">×</span>
+                        </button>
+                    </div>
+                @endif
+
+                <div class="p-6 text-gray-900 dark:text-gray-100">
+                    {{ __('Here you can manage your emails!') }}
+                </div>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
